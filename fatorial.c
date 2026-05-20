789cc23a33923ab2b0d6915b5221b995c36bc221
TravaChipzzxk <allysonrodolfozk@gmail.com>
2026-05-19 22:29:43 -0300

    Add files via upload

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    long skibidi = 1;
+
+    printf("digite um numero\n");
+    scanf("%d", &n);
+
+    skibidi = fatorial(n);
+
+    printf("o fatorial de %d é %d \n", n, skibidi);
+    printf("feito por benjamim messias");
+
+    return 0;
+}
